42267147805b35ad229dd0281b0137867c2e95d7
Dan Miller <dan@dmiller.dev>
2020-09-05 12:16:58 -0400

    why doesn't this work

--- cfreq/main.zig
+++ cfreq/main.zig
@@ -4,23 +4,20 @@ const io = std.io;
 const os = std.os;
 
 pub fn main() !void {
-    const allocator = std.heap.page_allocator;
+    const a = std.heap.page_allocator;
+    const map = std.AutoHashMap(u8, u64);
     const stdout = std.io.getStdOut().outStream();
-    try stdout.print("Hello, {}!\n", .{"world"});
-
     const stream = &std.io.getStdIn().inStream();
 
-    var repl_buf: [1024]u8 = undefined;
+    var buf: [1024]u8 = undefined;
 
-    while (try stream.readUntilDelimiterOrEof(&repl_buf, '\n')) |line| {
+    while (try stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         if (line.len == 0) {
             break;
         }
 
         for (line) |c| {
-            // TODO(dmiller): is there a better way to convert this to a string
-            var s = [1]u8{c};
-            try stdout.print("{}\n", .{s});
+            var entry = map.getOrPut(c);
         }
     }
 }

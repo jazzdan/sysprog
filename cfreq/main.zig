99f908f9caeacb846b934ea2f4688a8ce8f4721d
Dan Miller <dan@dmiller.dev>
2020-09-06 20:09:08 -0400

    at least this compiles

--- cfreq/main.zig
+++ cfreq/main.zig
@@ -5,11 +5,12 @@ const os = std.os;
 
 pub fn main() !void {
     const a = std.heap.page_allocator;
-    const map = std.AutoHashMap(u8, u64);
     const stdout = std.io.getStdOut().outStream();
     const stream = &std.io.getStdIn().inStream();
 
     var buf: [1024]u8 = undefined;
+    const Map = std.AutoHashMap(u8, u64);
+    var map = Map.init(a);
 
     while (try stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         if (line.len == 0) {
@@ -18,6 +19,8 @@ pub fn main() !void {
 
         for (line) |c| {
             var entry = map.getOrPut(c);
+            entry = entry + 1;
+            // TODO store back in map
         }
     }
 }

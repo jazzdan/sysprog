f9d72691ef113243c3508908a12bbfdbf30592cb
Dan Miller <dan@dmiller.dev>
2020-09-21 11:21:26 -0400

    this compiles

--- genealogy/main.zig
+++ genealogy/main.zig
@@ -4,7 +4,7 @@ const io = std.io;
 const os = std.os;
 
 const Person = struct {
-    name: []u8,
+    name: []const u8,
     mother: *Person,
     father: *Person,
 };
@@ -14,19 +14,21 @@ const Map = std.AutoHashMap([]u8, *Person);
 const stdin = std.io.getStdIn().inStream();
 const readUntil = stdin.readUntilDelimiterOrEof;
 
-fn handleField(fieldNum: u8, p: *Person, field: []u8) void {
+fn handleField(a: *std.mem.Allocator, fieldNum: u8, p: *Person, field: []const u8) void {
     if (fieldNum == 1) {
         p.name = field;
     } else if (fieldNum == 2) {
         // TODO(dmiller): insert in to hash map
-        p.father = &Person{
+        p.father = a.create(Person) catch @panic("Out of memory");
+        p.father.* = Person{
             .name = field,
             .mother = undefined,
             .father = undefined,
         };
     } else if (fieldNum == 3) {
         // TODO(dmiller): insert in to hash map
-        p.mother = &Person{
+        p.mother     = a.create(Person) catch @panic("Out of memory");
+        p.mother.* = Person{
             .name = field,
             .mother = undefined,
             .father = undefined,
@@ -49,7 +51,8 @@ pub fn main() !void {
     while (try readUntil(&buf, '\n')) |line| {
         try stdout.print("line {}\n", .{line});
         var iterator = std.mem.split(line, ",");
-        var p = &Person{
+        var p = a.create(Person) catch @panic("Out of memory");
+        p.* = Person{
             .name = undefined,
             .mother = undefined,
             .father = undefined,
@@ -57,7 +60,7 @@ pub fn main() !void {
         var fieldNum: u8 = 0;
         while (iterator.next()) |field| {
             try stdout.print("field {}\n", .{field});
-            handleField(fieldNum, p, field);
+            handleField(a, fieldNum, p, field);
             fieldNum = fieldNum + 1;
         }
     }

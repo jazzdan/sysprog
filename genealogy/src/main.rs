eeb632f9ee7e9f55c2a4541c92151af6f9bbf712
Dan Miller <dan@dmiller.dev>
2020-09-22 16:58:45 -0400

    this isn't quite right

--- genealogy/src/main.rs
+++ genealogy/src/main.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::io;
 use std::process;
+use std::collections::HashMap;
 
 use serde::Deserialize;
 
@@ -12,20 +13,51 @@ struct PersonRecord {
     mother: String,
 }
 
-fn example() -> Result<(), Box<dyn Error>> {
+struct Person {
+    name: String,
+    father: Option<*mut Person>,
+    mother: Option<*mut Person>,
+}
+
+fn parse() -> Result<std::vec::Vec<PersonRecord>, Box<dyn Error>> {
     let mut rdr = csv::Reader::from_reader(io::stdin());
+    let mut vec = Vec::new();
     for result in rdr.deserialize() {
         // Notice that we need to provide a type hint for automatic
         // deserialization.
         let record: PersonRecord = result?;
-        println!("{:?}", record);
+        vec.push(record);
     }
-    Ok(())
+    Ok(vec)
+}
+
+fn build_tree(recs: std::vec::Vec<PersonRecord>) -> HashMap<String, *mut Person> {
+    let mut map = HashMap::new();
+
+    for rec in recs {
+        let p = Person{
+            name: rec.name,
+            father: None,
+            mother: None,
+        };
+
+        let mp = &mut p;
+
+        map.insert(p.name, mp);
+    }
+
+    return map;
 }
 
 fn main() {
-    if let Err(err) = example() {
-        println!("error running example: {}", err);
-        process::exit(1);
+    let records = parse();
+    match records {
+        Ok(recs) => {
+
+        },
+        Err(err) => {
+            println!("Error parsing records: {:?}", err);
+            process::exit(1);
+        }
     }
 }

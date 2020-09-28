8f39f4d2c5422a229bfff11b3d038c80ee2c328a
Dan Miller <dan@dmiller.dev>
2020-09-28 11:02:50 -0400

    simple test & build setup

--- coursesdb/main.c
+++ coursesdb/main.c
@@ -4,6 +4,10 @@
 #include "coursesdb.h"
 
 int main() {
-  printf("Initializing database");
-  return init_database();
+  printf("Initializing database\n");
+  if (init_database() != 0) {
+    printf("Failed to initialize database\n");
+  } else {
+    printf("Successfully initialized database\n");
+  }
 }

b7e9ae890fa3e101bab03085d970ea4cc160bfed
Dan Miller <dan@dmiller.dev>
2020-09-29 16:10:54 -0400

    wip

--- genealogy/src/main.rs
+++ genealogy/src/main.rs
@@ -1,7 +1,7 @@
+use std::collections::HashMap;
 use std::error::Error;
 use std::io;
 use std::process;
-use std::collections::HashMap;
 
 use serde::Deserialize;
 
@@ -13,11 +13,11 @@ struct PersonRecord {
     mother: String,
 }
 
-struct Person {
-    name: String,
-    father: Option<*mut Person>,
-    mother: Option<*mut Person>,
-}
+// struct Person {
+//     name: String,
+//     father: Option<*mut Person>,
+//     mother: Option<*mut Person>,
+// }
 
 fn parse() -> Result<std::vec::Vec<PersonRecord>, Box<dyn Error>> {
     let mut rdr = csv::Reader::from_reader(io::stdin());
@@ -31,33 +31,120 @@ fn parse() -> Result<std::vec::Vec<PersonRecord>, Box<dyn Error>> {
     Ok(vec)
 }
 
-fn build_tree(recs: std::vec::Vec<PersonRecord>) -> HashMap<String, *mut Person> {
+fn build_tree(recs: std::vec::Vec<PersonRecord>) -> HashMap<String, &'static mut Person<'static>> {
     let mut map = HashMap::new();
 
     for rec in recs {
-        let p = Person{
-            name: rec.name,
+        let mut p = Person{
+            name: &rec.name,
             father: None,
             mother: None,
         };
 
         let mp = &mut p;
 
-        map.insert(p.name, mp);
+        map.insert(rec.name, mp);
     }
 
     return map;
 }
 
-fn main() {
-    let records = parse();
-    match records {
-        Ok(recs) => {
-
-        },
-        Err(err) => {
-            println!("Error parsing records: {:?}", err);
-            process::exit(1);
+#[derive(PartialEq)]
+struct Person<'a> {
+    name: &'a str,
+    mother: Option<Box<Person<'a>>>,
+    father: Option<Box<Person<'a>>>,
+}
+impl<'a> Person<'a> {
+    pub fn insert(&mut self, new_val: &'a str) {
+        if self.name== new_val {
+            return;
+        }
+        let target_node = if new_val < self.name {
+            &mut self.mother
+        } else {
+            &mut self.father
+        };
+        match target_node {
+            &mut Some(ref mut subnode) => subnode.insert(new_val),
+            &mut None => {
+                let new_node = Person {
+                    name: new_val,
+                    mother: None,
+                    father: None,
+                };
+                let boxed_node = Some(Box::new(new_node));
+                *target_node = boxed_node;
+            }
         }
     }
+
+    pub fn set_mother(&mut self, mother: &'a str) {
+        let target_person = &mut self.mother;
+
+        let new_person = Person {
+            name: mother,
+            mother: None,
+            father: None,
+        };
+
+        let boxed_person = Some(Box::new(new_person));
+        *target_person = boxed_person;
+    }
+
+    pub fn set_father(&mut self, father: &'a str) {
+        let target_person = &mut self.father;
+
+        let new_person = Person {
+            name: father,
+            mother: None,
+            father: None,
+        };
+
+        let boxed_person = Some(Box::new(new_person));
+        *target_person = boxed_person;
+    }
 }
+
+fn main() {
+    let mut x = Person {
+        name: "m",
+        mother: None,
+        father: None,
+    };
+    x.insert("z");
+    x.insert("b");
+    x.insert("c");
+    assert!(
+        x == Person {
+            name: "m",
+            mother: Some(Box::new(Person {
+                name: "b",
+                mother: None,
+                father: Some(Box::new(Person {
+                    name: "c",
+                    mother: None,
+                    father: None
+                })),
+            })),
+            father: Some(Box::new(Person {
+                name: "z",
+                mother: None,
+                father: None
+            })),
+        }
+    );
+}
+
+// fn main() {
+//     let records = parse();
+//     match records {
+//         Ok(recs) => {
+
+//         },
+//         Err(err) => {
+//             println!("Error parsing records: {:?}", err);
+//             process::exit(1);
+//         }
+//     }
+// }
